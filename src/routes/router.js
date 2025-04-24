7a8ef524bcfe3cabe0f9b4f0a38a69259a7883fc
JoannZamudio11 <126726984+JoannZamudio11@users.noreply.github.com>
2025-04-24 15:30:06 -0500

    Integration of the widget in the heatmap module and the API in the news module
    This pull request integrates the new widget into the heatmap module and connects the API within the news module. These updates enhance the functionality and interactivity of both components.
    
    Note: The transition between dark and light mode still needs to be fixed.

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,9 +1,11 @@
 const express = require('express')
-const {mainView, tables, notificationsUser} = require('../controllers/PageControllers')
+const {mainView, tables, notificationsUser, heatMap, news} = require('../controllers/PageControllers')
 const router = express.Router()
 
 router.get('/', mainView)
 router.get('/tables', tables)
 router.get('/notifications', notificationsUser)
+router.get('/heatmap', heatMap)
+router.get('/news', news)
 
 module.exports = {routes:router}
